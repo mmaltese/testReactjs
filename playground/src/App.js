a606f1cf418046d5287bce0e5d2bf4042adf4194
mare <mare.maltese@gmail.com>
2018-06-30 01:01:27 +0200

    Using props to pass a list

--- playground/src/App.js
+++ playground/src/App.js
@@ -5,8 +5,17 @@ import { Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent'
 
 import './App.css';
+import { DISHES } from './shared/dishes'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,7 +24,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante con fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }

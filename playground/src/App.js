c4952fc08db9e5e89fecb4690695874044d926f5
mare <mare.maltese@gmail.com>
2018-06-30 11:58:16 +0200

    First try new component DishDetails

--- playground/src/App.js
+++ playground/src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 
 import { Navbar, NavbarBrand} from 'reactstrap';
-import Menu from './components/MenuComponent'
+import Menu from './components/MenuComponent';
 
 import './App.css';
-import { DISHES } from './shared/dishes'
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
 

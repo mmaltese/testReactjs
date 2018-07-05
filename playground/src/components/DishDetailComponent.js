1a3faf202be7c75561eefe70901049e74034fd6d
mare <mare.maltese@gmail.com>
2018-07-05 18:53:26 +0200

    functional components

--- playground/src/components/DishDetailComponent.js
+++ playground/src/components/DishDetailComponent.js
@@ -1,17 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-
-class DishDetail extends Component {
-
-    componentDidMount() {
-        console.log('DishDetail component componentDidMount is invoked');
-    }
-
-    componentDidUpdate() {
-        console.log('DishDetail component componentDidUpdate is invoked');
-    }
     
-    renderComments(comments) {
+    function RenderComments({comments}) {
         if (comments != null) {
             return(
                 <div className="col-12 col-md-5 m-1">
@@ -35,7 +25,7 @@ class DishDetail extends Component {
         }
     }
 
-    renderDish(dish) {
+    function RenderDish({dish}) {
             return(
                 <div className="col-12 col-md-5 m-1">
                     <Card>
@@ -49,14 +39,14 @@ class DishDetail extends Component {
             );
     }
 
-    render() {
+    const DishDetail = (props) => {
         console.log('DishDetail component render invoked');
-        if (this.props.dish != null)
+        if (props.dish != null)
             return (
                 <div className="container">
                     <div className="row">
-                        {this.renderDish(this.props.dish)}
-                        {this.renderComments(this.props.dish.comments)}
+                        <RenderDish dish={props.dish}/>
+                        <RenderComments comments={props.dish.comments} />
                     </div>
                 </div>
             );
@@ -65,6 +55,5 @@ class DishDetail extends Component {
                 <div></div>
             );
     }
-}
 
 export default DishDetail;
--- playground/src/components/MenuComponent.js
+++ playground/src/components/MenuComponent.js
@@ -1,36 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Menu extends Component {
-
-    constructor(props) {
-        super(props);
-
-        console.log('menu component contructor is invoked');
+    function RenderMenuItem({dish, onClick}) {
+        return(
+            <Card onClick={() => onClick(dish.id)}>
+                <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+                <CardImgOverlay body="true" className="ml-5">
+                    <CardTitle heading="true">{dish.name}</CardTitle>
+                    <p>{dish.description}</p>
+                </CardImgOverlay>
+            </Card>
+        );
     }
+    
+    const Menu = (props) => {
 
-    componentDidMount() {
-        console.log('menu component componentDidMount is invoked');
-    }
-
-    render() {
-
-        const menu = this.props.dishes.map((dish) => {
+        const menu = props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.props.onClick(dish.id)}>
-                        <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
-                        <CardImgOverlay body="true" className="ml-5">
-                            <CardTitle heading="true">{dish.name}</CardTitle>
-                            <p>{dish.description}</p>
-                        </CardImgOverlay>
-                    </Card>
+                    <RenderMenuItem dish={dish} onClick={props.onClick} />
                 </div>
             );
         }); 
-
-
-        console.log('menu component render is invoked');
 
         return (
             <div className="container">
@@ -39,8 +30,7 @@ class Menu extends Component {
                 </div>
             </div>
         );
-    }
 
-}
+    }
 
 export default Menu;
